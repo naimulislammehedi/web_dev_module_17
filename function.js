72a4aea8ebac64d61af08ba44992121d0ea3167d
Mehedi <mehedi.interface@gmail.com>
2026-01-31 23:12:21 +0600

    17-1 What are function and function syntax

---
+++ function.js
@@ -0,0 +1,18 @@
+const number = 45; 
+
+function fanOffKor() {
+    console.log("Bosa theke uthe dara"); 
+    console.log("Walk towards the switch"); 
+    console.log("Click the switch to off of the Fan");
+}
+
+fanOffKor(); 
+
+function brushTeeth() {
+    console.log("Pick up the brush"); 
+    console.log("Add paste"); 
+    console.log("2 min ghoso and ghoso and fena felo"); 
+}
+
+brushTeeth(); 
+
