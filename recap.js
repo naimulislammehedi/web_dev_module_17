036334c02b6c206829f71bc29976c636efadfb4b
Mehedi <mehedi.interface@gmail.com>
2026-02-01 10:22:40 +0600

    17-6 Recap and conditional return of odd and even

---
+++ recap.js
@@ -0,0 +1,27 @@
+function add(price1, price2) {
+    const total = price1 + price2; 
+    return total; 
+}
+
+const bill = add(5, 10)
+console.log(bill); 
+
+
+function add2 (price1, price2) {
+    return price1 + price2; 
+}
+const bill2 = add2(10, 20); 
+console.log(bill2); 
+
+
+function doMath(num1, num2) {
+    const sum = num1 + num2; 
+    const diff = num1 - num2; 
+    const mult = sum * diff; 
+    const result = mult / 2; 
+    return result; 
+}
+
+const result = doMath(20, 8); 
+console.log(result); 
+
