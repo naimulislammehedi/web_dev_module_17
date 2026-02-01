d896678b6ed28faed7c2afc206263883f125f1f7
Mehedi <mehedi.interface@gmail.com>
2026-02-01 10:04:40 +0600

    17-5 Function return and set return value to a variable

---
+++ return.js
@@ -0,0 +1,12 @@
+function TenTimes(number) {
+    const result = number * 10; 
+    return result; 
+}
+
+function cutHalf(number) {
+    const half = number / 2; 
+    return half; 
+}
+
+const output = TenTimes(10); 
+console.log('output', output)
